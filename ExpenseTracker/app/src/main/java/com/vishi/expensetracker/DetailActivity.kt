d75016a6085590f24746f296fc8fe01a41e40205
unknown <bvishwas21@gmail.com>
2019-11-28 22:38:33 +0530

    Month and Previous Day Total

--- ExpenseTracker/app/src/main/java/com/vishi/expensetracker/DetailActivity.kt
+++ ExpenseTracker/app/src/main/java/com/vishi/expensetracker/DetailActivity.kt
@@ -7,12 +7,15 @@ import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.DocumentChange
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ListenerRegistration
 import com.vishi.expensetracker.model.ExpenseDetails
+import java.text.SimpleDateFormat
 import java.util.*
+
 
 class DetailActivity : AppCompatActivity() {
 
@@ -20,9 +23,18 @@ class DetailActivity : AppCompatActivity() {
     private var mYear: Int? = 0
     private var mMonth: Int? = 0
     private var mDay: Int? = 0
+    private var mMonthName: String? = ""
+    private var mYestYear: Int? = 0
+    private var mYestMonth: Int? = 0
+    private var mYestDay: Int? = 0
+    private var mYestMonthName: String? = ""
 
     private lateinit var totalDayExpense: TextView
     private lateinit var addExpenseButton: TextView
+    private lateinit var yesterdayTotalExpense: TextView
+    private lateinit var yesterdayNameLabel: TextView
+    private lateinit var totalMonthExpense: TextView
+    private lateinit var monthNameLabel: TextView
 
     val databaseReference: FirebaseFirestore = FirebaseFirestore.getInstance()
     private lateinit var dbListener: ListenerRegistration
@@ -37,17 +49,46 @@ class DetailActivity : AppCompatActivity() {
         mMonth = mCal!!.get(Calendar.MONTH)
         mDay = mCal!!.get(Calendar.DAY_OF_MONTH)
 
+        val cal = Calendar.getInstance()
+        val month_date = SimpleDateFormat("MMMM")
+        cal.set(Calendar.MONTH, mMonth!!)
+        mMonthName = month_date.format(cal.time)
+
+        val yesCal = yesterday()
+        mYestYear = yesCal.get(Calendar.YEAR)
+        mYestMonth = yesCal.get(Calendar.MONTH)
+        mYestDay = yesCal.get(Calendar.DAY_OF_MONTH)
+
+        cal.set(Calendar.MONTH, mYestMonth!!)
+        mYestMonthName = month_date.format(cal.time)
+
         totalDayExpense = findViewById(R.id.totalDayExpensesId)
         addExpenseButton = findViewById(R.id.addExpenseButtonId)
         addExpenseButton.isEnabled = false
 
+        yesterdayTotalExpense = findViewById(R.id.totalYesterdayExpenseId)
+        yesterdayNameLabel = findViewById(R.id.yesterdayNameLabelId)
+
+        totalMonthExpense = findViewById(R.id.totalMonthlyExpenseId)
+        monthNameLabel = findViewById(R.id.monthNameLabelId)
+
         getExpensesDetails()
 
         addExpenseButton.setOnClickListener {
-            var add = Intent(this@DetailActivity, AddExpenseActivity::class.java)
+            val add = Intent(this@DetailActivity, AddExpenseActivity::class.java)
             startActivityForResult(add, ADD_EXPENSE_REQUEST)
         }
 
+        updateViews()
+    }
+
+    private fun yesterday(): Calendar {
+        val cal = Calendar.getInstance()
+        cal.set(Calendar.YEAR, mYear!!)
+        cal.set(Calendar.MONTH, mMonth!!)
+        cal.set(Calendar.DAY_OF_MONTH, mDay!!)
+        cal.add(Calendar.DATE, -1)
+        return cal
     }
 
     override fun onDestroy() {
@@ -59,9 +100,6 @@ class DetailActivity : AppCompatActivity() {
         val uid = FirebaseAuth.getInstance().currentUser!!.uid
 
         dbListener = databaseReference.collection("expenses")
-            .whereEqualTo("day", mDay)
-            .whereEqualTo("month", (mMonth!! + 1))
-            .whereEqualTo("year", mYear!!)
             .whereEqualTo("addedBy", uid)
             .addSnapshotListener { querySnapshot, firebaseFirestoreException ->
                 if (firebaseFirestoreException != null) {
@@ -73,7 +111,7 @@ class DetailActivity : AppCompatActivity() {
                     for (dc in querySnapshot.documentChanges) {
                         when (dc.type) {
                             DocumentChange.Type.ADDED -> {
-                                var newExpense = dc.document.toObject(ExpenseDetails::class.java)
+                                val newExpense = dc.document.toObject(ExpenseDetails::class.java)
                                 newExpense.id = dc.document.id
 
                                 expenseList.add(newExpense)
@@ -82,7 +120,7 @@ class DetailActivity : AppCompatActivity() {
                             }
 
                             DocumentChange.Type.MODIFIED -> {
-                                var modifiedExpense = dc.document.toObject(ExpenseDetails::class.java)
+                                val modifiedExpense = dc.document.toObject(ExpenseDetails::class.java)
                                 modifiedExpense.id = dc.document.id
 
                                 updateExpense(modifiedExpense)
@@ -91,7 +129,7 @@ class DetailActivity : AppCompatActivity() {
                             }
 
                             DocumentChange.Type.REMOVED -> {
-                                var deletedExpense = dc.document.toObject(ExpenseDetails::class.java)
+                                val deletedExpense = dc.document.toObject(ExpenseDetails::class.java)
                                 deletedExpense.id = dc.document.id
 
                                 deleteExpense(deletedExpense)
@@ -106,15 +144,62 @@ class DetailActivity : AppCompatActivity() {
 
     private fun updateViews() {
         var dayTotal = 0.0
+        var yestDayTotal = 0.0
+        var monthTotal = 0.0
+        var yearTotal = 0.0
 
         for (expense in expenseList) {
-            if (expense.day == mDay) {
-                dayTotal += expense.amountSpent
+            if (expense.year == mYear!!) {
+                if (expense.month == (mMonth!! + 1)) {
+                    if (expense.day == mDay!!) {
+                        dayTotal += expense.amountSpent
+                    }
+                    monthTotal += expense.amountSpent
+                }
+                yearTotal += expense.amountSpent
+            }
+
+            if (expense.year == mYestYear!! && expense.month == (mYestMonth!! + 1) && expense.day == mYestDay!!) {
+                yestDayTotal += expense.amountSpent
             }
         }
 
-        totalDayExpense.text = "\u20B9 " +  String.format(Locale.ENGLISH, "%.2f", dayTotal)
+        totalDayExpense.text = "\u20B9" +  String.format(Locale.ENGLISH, "%.2f", dayTotal)
         addExpenseButton.isEnabled = true
+
+        yesterdayTotalExpense.text = "\u20B9" + String.format(Locale.ENGLISH, "%.2f", yestDayTotal)
+        var suffix: String = ""
+
+        if (mYestDay!! >= 11 && mYestDay!! <= 20) {
+            suffix = "th"
+        }
+        else if (mYestDay!! % 10 == 1) {
+            suffix = "st"
+        }
+        else if (mYestDay!! % 10 == 2) {
+            suffix = "nd"
+        }
+        else if (mYestDay!! % 10 == 3) {
+            suffix = "rd"
+        }
+        else {
+            suffix = "th"
+        }
+
+        yesterdayNameLabel.setText("" + mYestDay + suffix + " " + mYestMonthName + ", " + mYestYear)
+
+        totalMonthExpense.text = "\u20B9" + String.format(Locale.ENGLISH, "%.2f", monthTotal)
+        monthNameLabel.text = mMonthName
+
+        if (monthTotal <= 10000) {
+            totalMonthExpense.setTextColor(ContextCompat.getColor(this@DetailActivity, R.color.colorSafeGreen))
+        }
+        else if (monthTotal <= 25000) {
+            totalMonthExpense.setTextColor(ContextCompat.getColor(this@DetailActivity, R.color.colorWarningOrange))
+        }
+        else {
+            totalMonthExpense.setTextColor(ContextCompat.getColor(this@DetailActivity, R.color.colorDangerRed))
+        }
     }
 
     private fun deleteExpense(deletedExpense: ExpenseDetails) {
@@ -124,7 +209,7 @@ class DetailActivity : AppCompatActivity() {
             if (exp.id == deletedExpense.id) {
                 break
             }
-            index = index + 1
+            index += 1
         }
 
         if (index != -1 && index < expenseList.size) {
@@ -139,7 +224,7 @@ class DetailActivity : AppCompatActivity() {
             if (exp.id == modifiedExpense.id) {
                 break
             }
-            index = index + 1
+            index += 1
         }
 
         if (index != -1 && index < expenseList.size) {
